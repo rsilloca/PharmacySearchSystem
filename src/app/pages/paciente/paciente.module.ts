03a70aca15d058fc7229143c86e30fb7aca928dd
rsilloca <rsilloca@unsa.edu.pe>
2020-11-29 12:11:44 -0500

    paciente module config

--- src/app/pages/paciente/paciente.module.ts
+++ src/app/pages/paciente/paciente.module.ts
@@ -6,10 +6,11 @@ import { BusquedaComponent } from './busqueda/busqueda.component';
 import { MaterialComponentsModule } from 'src/@theming/shared/material-components.module';
 import { FiltroComponent } from './busqueda/filtro/filtro.component';
 import { SharedModule } from 'src/@theming/shared.module';
+import { DetallesProductoComponent } from './busqueda/detalles-producto/detalles-producto.component';
 
 
 @NgModule({
-  declarations: [BusquedaComponent, FiltroComponent],
+  declarations: [BusquedaComponent, FiltroComponent, DetallesProductoComponent],
   imports: [
     CommonModule,
     PacienteRoutingModule,
