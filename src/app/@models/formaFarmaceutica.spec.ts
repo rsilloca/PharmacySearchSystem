a876623c84afc25433fbbd9064cbeca244ffcd9f
Maryure Layme <mlaymea@unsa.edu.pe>
2020-12-07 21:52:30 -0500

    'formaFarmaceutica'

---
+++ src/app/@models/formaFarmaceutica.spec.ts
@@ -0,0 +1,7 @@
+import { FormaFarmaceutica } from './formaFarmaceutica';
+
+describe('FormaFarmaceutica', () => {
+  it('should create an instance', () => {
+    expect(new FormaFarmaceutica()).toBeTruthy();
+  });
+});
--- src/app/@models/producto.ts
+++ src/app/@models/producto.ts
@@ -1,2 +1,33 @@
-export class Producto {
+import {Generico} from './generico';
+import {Parametro} from './parametro';
+import {FormaFarmaceutica} from './FormaFarmaceutica';
+import {Categoria} from './categoria';
+import {Farmacia} from './farmacia';
+
+export class Producto extends Generico {
+    idProducto: number;
+    idCategoria: number;
+    idParametro:number;
+    idFarmacia:number;
+    codigo:string;
+    valor: string;
+    stock:number;
+    precioUnitario: number;
+    marca: string;
+    idFormaFarmaceutica: number;
+
+    constructor(obj?:Producto){
+        super()
+        this.idCategoria = obj && obj.idCategoria ? obj.idCategoria : 0;
+        this.idParametro = obj && obj.idParametro ? obj.idParametro : 0;
+        this.idFormaFarmaceutica = obj && obj.idFormaFarmaceutica ? obj.idFormaFarmaceutica : 0;
+        this.codigo = obj && obj.codigo ? obj.codigo : "";
+        this.valor = obj && obj.valor ? obj.valor : "";
+        this.stock = obj && obj.stock ? obj.stock : 0;
+        this.precioUnitario = obj && obj.precioUnitario ? obj.precioUnitario : 0;
+        this.marca = obj && obj.marca ? obj.marca :"";
+      
+      
+    }
 }
+
