9444428932125cc74ecea9695cbf00fa90b61fe2
rsilloca <rsilloca@unsa.edu.pe>
2020-12-09 19:17:54 -0500

    Update ApiKey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: '',
+  googleMapsApiKey: 'AIzaSyBdUeyFcBO969YQ5P_0ghKRj3WzqrWeqEQ',
   backend: '', // Put your backend here
 };
